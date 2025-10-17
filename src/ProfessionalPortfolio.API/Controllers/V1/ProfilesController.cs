43d96d0da7c5643d8b5e5d8c7771e43a9107fe11
Toba Ojo <ojotobar@gmail.com>
2025-10-17 09:58:48 +0100

    Added the projects

---
+++ src/ProfessionalPortfolio.API/Controllers/V1/ProfilesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProfessionalPortfolio.API.Controllers.V1
+{
+    [Route("api/v1/profiles")]
+    [ApiController]
+    public class ProfilesController : ControllerBase
+    {
+    }
+}
